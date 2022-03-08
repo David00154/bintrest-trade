e45843449d692c594c83cf52e5928998bf7a08e6
David00154 <davidbriggs00154@gmail.com>
2022-03-08 02:31:34 +0100

    lil change

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "static")));
 app.use(expressLayouts);
 app.set("view engine", "ejs");
-app.set('views', join(process.cwd(), "views"))
+app.set('views', path.join(process.cwd(), "views"))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
