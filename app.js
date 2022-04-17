d4f3d3a84de116ffaf29b2eac574af6aac75120f
David00154 <davidbriggs00154@gmail.com>
2022-04-17 20:02:36 +0100

    made lil changes to env

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv");
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
--- utils/supabase.js
+++ utils/supabase.js
@@ -1,7 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
 
-
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabase = createClient(
+	"https://gqqxcbijpiixtegemasz.supabase.co",
+	process.env.SUPABASE_KEY
+);
 
 module.exports = supabase;
