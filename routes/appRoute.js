e4420cc5e417467b003b0abd8254a4475e35513c
David Briggs <davidbriggs00154@gmail.com>
2023-02-15 20:38:22 +0000

    changed to quodra fx

--- routes/appRoute.js
+++ routes/appRoute.js
@@ -3,14 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-	res.render("index", { title: "Coivest Capital" });
+	res.render("index", { title: "Quodra Fx" });
 });
 router.route("/about").get((req, res) => {
-	res.render("about", { title: "About - Coivest Capital" });
+	res.render("about", { title: "About - Quodra Fx" });
 	// res.redirect("/")
 });
 router.route("/contact-us").get((req, res) => {
-	res.render("contact", { title: "Contact us - Coivest Capital" });
+	res.render("contact", { title: "Contact us - Quodra Fx" });
 });
 
 module.exports = router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router
 	.route("/login")
 	.get(forwardAuthenticated, (req, res) => {
-		res.render("login", { title: "Log in - Coivest Capital" });
+		res.render("login", { title: "Log in - Quodra Fx" });
 	})
 	.post(login);
 
@@ -22,7 +22,7 @@ router.route("/authorize").get((req, res) => {
 router
 	.route("/signup")
 	.get((req, res) => {
-		res.render("signup", { title: "Sign up - Coivest Capital" });
+		res.render("signup", { title: "Sign up - Quodra Fx" });
 	})
 	.post(
 		validateSignupFields,
