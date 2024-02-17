b3464c4d65cd96293d68697e883b7cee5a0eae3e
david00154 <davidbriggs00154@gmail.com>
2024-02-17 20:49:24 +0100

    fixed session issue

--- app.js
+++ app.js
@@ -10,11 +10,17 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const { __passport__ } = require("./utils/passport.js");
 const MongoStore = require("connect-mongo");
+const pg = require("pg");
 
 // import appRoute from "./routes/appRoute.js";
 // import userRoute from "./routes/userRoute.js";
 // import adminRoute frp
-const { adminRoute, userRoute, appRoute, dashboardRoute } = require("./routes/index.js");
+const {
+  adminRoute,
+  userRoute,
+  appRoute,
+  dashboardRoute,
+} = require("./routes/index.js");
 
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
@@ -35,21 +41,26 @@ app.use(express.urlencoded({ extended: true }));
 
 // Express session
 app.use(
-	session({
-		secret: "secret",
-		resave: false,
-		saveUninitialized: false,
-		// store: MongoStore.create({
-		// 	mongoUrl: "mongodb://u4fakmxzdkt0pd4msndg:6WDZB5FOhYXzK3jIA3mj@bqydofpk5iljqqq-mongodb.services.clever-cloud.com:27017/bqydofpk5iljqqq",
-		// 	dbName: "bqydofpk5iljqqq",
-		// 	autoRemove: "disabled",
-		// 	touchAfter: 24 * 3600
-		// }),
-		store: new (require("connect-pg-simple")(session))({
-			createTableIfMissing: true,
-		}),
-		cookie: { maxAge: 2 * 24 * 60 * 60 * 1000 },
-	})
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    // store: MongoStore.create({
+    // 	mongoUrl: "mongodb://u4fakmxzdkt0pd4msndg:6WDZB5FOhYXzK3jIA3mj@bqydofpk5iljqqq-mongodb.services.clever-cloud.com:27017/bqydofpk5iljqqq",
+    // 	dbName: "bqydofpk5iljqqq",
+    // 	autoRemove: "disabled",
+    // 	touchAfter: 24 * 3600
+    // }),
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pruneSessionInterval: false,
+      //   pool: new pg.Pool({
+      //     password:
+      //       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxcXhjYmlqcGlpeHRlZ2VtYXN6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MDIwNjkxMCwiZXhwIjoxOTY1NzgyOTEwfQ.bi6MLGDQR9nqPHMZL21-nnjlzyYCfry4rVpbC_YBjTM",
+      //   }),
+    }),
+    cookie: { maxAge: 2 * 24 * 60 * 60 * 1000 },
+  })
 );
 //
 // Passport middleware
@@ -61,10 +72,10 @@ app.use(flash());
 
 // Global variables
 app.use(function (req, res, next) {
-	res.locals.success_msg = req.flash("success_msg");
-	res.locals.error_msg = req.flash("error_msg");
-	res.locals.error = req.flash("error");
-	next();
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
 });
 //
 // routes
@@ -73,12 +84,11 @@ app.use("/user", userRoute);
 app.use("/admin", adminRoute);
 app.use("/dashboard", dashboardRoute);
 app.get("/auth/logout", (req, res) => {
-	req.session.current_url = "";
-	req.logout();
-	req.flash("success_msg", "You are logged out");
-	res.redirect("/user/login");
+  req.session.current_url = "";
+  req.logout();
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/user/login");
 });
-
 
 //app.get("*", (req, res) => {
 //	res.render("500", { layout: false });
@@ -90,10 +100,10 @@ app.get("/auth/logout", (req, res) => {
 // 	);
 // });
 
-const port = process.env.PORT || 5555;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
 
 module.exports = app;
--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 export SUPABASE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxcXhjYmlqcGlpeHRlZ2VtYXN6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MDIwNjkxMCwiZXhwIjoxOTY1NzgyOTEwfQ.bi6MLGDQR9nqPHMZL21-nnjlzyYCfry4rVpbC_YBjTM
 export SUPABASE_URL=https://gqqxcbijpiixtegemasz.supabase.co
-export DATABASE_URL=postgresql://postgres:davidbriggs00154@gmail.com@db.gqqxcbijpiixtegemasz.supabase.co:5432/postgres
+# export DATABASE_URL=postgresql://postgres:davidbriggs00154@gmail.com@db.gqqxcbijpiixtegemasz.supabase.co:5432/postgres
+export DATABASE_URL=postgres://postgres.gqqxcbijpiixtegemasz:davidbriggs00154@gmail.com@aws-0-us-east-1.pooler.supabase.com:5432/postgres
+
+
 
 npm run dev:watch
